df56ae72210223b2a93d031d6f8fdfa7a28eb381
LAPTOP-1E1JPDL3\User <ekarat.suk@hotmail.com>
2024-06-28 16:57:24 +0700

    SET CODE FOR IP_ADDRESS AND ON VM

--- TerminalChat/Program.cs
+++ TerminalChat/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+
+class Sender { 
+    static void Main(String[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter messagge here");
+            TcpClient client = new TcpClient();
+            NetworkStream stream = client.GetStream();
+            StreamWriter writer = new StreamWriter(stream);
+            string message = Console.ReadLine();
+            while(message != null)
+            {
+                writer.WriteLine(message);
+                writer.Flush();
+            }
+            
+        }
+        catch (Exception e)
+        {
+            Console.Write(e.Message);
+        }
+    }
+}
